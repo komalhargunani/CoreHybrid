74c350b752c28c7e995969f61dedebacf6c48996
komalh <komalh@ULTP_649.synerzipune.local>
2017-11-08 17:28:47 +0530

    updating

--- CoreHybrid1/src/main/java/com/corehybrid/utils/Constants.java
+++ CoreHybrid1/src/main/java/com/corehybrid/utils/Constants.java
@@ -3,5 +3,10 @@ package com.corehybrid.utils;
 public class Constants {
 
 	public static final String SUITA_XLS = "E:\\proxibid_automation\\ProxibidAutomation\\CoreHybrid\\data\\SuiteA.xlsx";
+	
 	public static final String KEYWORDS_SHEET = "Keywords";
+
+	public static final String BUYER_XLS = "E:\\proxibid_automation\\ProxibidAutomation\\CoreHybrid\\data\\CoreBuyer.xlsx";
+
+	public static final String Buyer_SHEET_LOGIN = "BuyerLogin";
 }
