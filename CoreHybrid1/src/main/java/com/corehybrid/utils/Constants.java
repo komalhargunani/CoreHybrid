89fb61cc8ab35be7d1cbf52da4e8d321fbc932e3
komalh <komalh@ULTP_649.synerzipune.local>
2017-11-08 15:38:28 +0530

    first time

---
+++ CoreHybrid1/src/main/java/com/corehybrid/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.corehybrid.utils;
+
+public class Constants {
+
+	public static final String SUITA_XLS = "E:\\proxibid_automation\\ProxibidAutomation\\CoreHybrid\\data\\SuiteA.xlsx";
+	public static final String KEYWORDS_SHEET = "Keywords";
+}
